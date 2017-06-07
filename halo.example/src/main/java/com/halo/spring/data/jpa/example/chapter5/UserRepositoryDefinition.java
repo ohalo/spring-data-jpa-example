afea6d0821200efcb22b4bd7357a2394453e9b3e
zhaohuiliang <zhaohuiliang@chexiang.com>
2017-06-07 10:56:27 +0800

    实现注解继承

---
+++ halo.example/src/main/java/com/halo/spring/data/jpa/example/chapter5/UserRepositoryDefinition.java
@@ -0,0 +1,17 @@
+package com.halo.spring.data.jpa.example.chapter5;
+
+import org.springframework.data.repository.RepositoryDefinition;
+
+import com.halo.spring.data.jpa.example.chapter4.User;
+
+@RepositoryDefinition(domainClass = User.class, idClass = Long.class)
+public interface UserRepositoryDefinition {
+
+    /**
+     * Spring data 根据方法名规则的定义查询用户信息
+     * 
+     * @param username
+     * @return
+     */
+    User findByUsername(String username);
+}
